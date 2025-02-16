072a09cec2f4362df09c7ca992fcb7d31be68aa8
Kuldeep kaushik <45338143+kuldeep101990@users.noreply.github.com>
2025-02-16 16:33:26 +0530

    Update FilterPersonsByName.java

--- src/main/java/hql/FilterPersonsByName.java
+++ src/main/java/hql/FilterPersonsByName.java
@@ -33,7 +33,7 @@ public class FilterPersonsByName {
             
             // Execute a query to filter Person records by name pattern
             Query<Person> query = session.createQuery("FROM Person WHERE name LIKE :name", Person.class);
-            query.setParameter("name", "%John%");
+            query.setParameter("name", "%amit%");
             
             // Get the list of persons whose name matches the pattern
             List<Person> filteredPersons = query.list();
